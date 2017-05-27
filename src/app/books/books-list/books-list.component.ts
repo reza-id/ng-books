2b29c603f8c018e37d7fb0704d4c57e972ab1460
Reza Agustiana <reza.agustiana@gmail.com>
2017-05-27 10:28:21 +0700

    pipes

--- src/app/books/books-list/books-list.component.ts
+++ src/app/books/books-list/books-list.component.ts
@@ -11,27 +11,30 @@ export class BooksListComponent implements OnInit {
     {
       bookAuthor: 'Tom Jones',
       bookTitle: 'War and Peace 2',
-      bookPrice: '$29.95',
+      bookPrice: 29.95,
       bookDescription: 'Book of historical fiction',
       bookReviews: 15,
+      publishedOn: new Date('02/11/1921'),
       bookImageUrl: 'assets/images/656.jpg',
-      bookInStock: 2
+      bookInStock: 'yes'
     },{
       bookAuthor: 'Tom Jones',
       bookTitle: 'War and Peace 2',
-      bookPrice: '$29.95',
+      bookPrice: 29.95,
       bookDescription: 'Book of historical fiction',
       bookReviews: 15,
+      publishedOn: new Date('02/11/1921'),
       bookImageUrl: 'assets/images/656.jpg',
-      bookInStock: 2
+      bookInStock: 'yes'
     },{
       bookAuthor: 'Tom Jones',
       bookTitle: 'War and Peace 2',
-      bookPrice: '$29.95',
+      bookPrice: 29.95,
       bookDescription: 'Book of historical fiction',
       bookReviews: 15,
+      publishedOn: new Date('02/11/1921'),
       bookImageUrl: 'assets/images/656.jpg',
-      bookInStock: 2
+      bookInStock: 'yes'
     }
   ];
 
