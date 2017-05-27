244ca872936ec09f40752b4b550ae56839d1472e
Reza Agustiana <reza.agustiana@gmail.com>
2017-05-27 16:43:45 +0700

    observable handle error

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -3,6 +3,8 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { IBook } from './book';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class BookService {
@@ -12,6 +14,14 @@ export class BookService {
     getBooks(): Observable<IBook[]> {
         return this._http
             .get('assets/books.json')
-            .map((response: Response) => <IBook[]> response.json());
+            .map((response: Response) => <IBook[]> response.json())
+            .do(data => console.log(data))
+            .catch(this.handleError);
+    }
+
+    private handleError(error: Response) {
+        console.error(error);
+        let message = `Error status code ${error.status} at ${error.url}`;
+        return Observable.throw(message);
     }
 }
